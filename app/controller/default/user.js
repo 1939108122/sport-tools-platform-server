22a72d6fb89e0dbe735be0283d167c8c5d4caabf
1939108122 <1939108122@qq.com>
2021-04-24 20:47:50 +0800

    登录模块接口

--- app/controller/default/user.js
+++ app/controller/default/user.js
@@ -6,17 +6,67 @@ class UserController extends Controller {
 
   // 登录
   async login() {
-    let sql = 'select * from user';
-    const result = await this.app.mysql.query(sql);
-    this.ctx.body = result;
-
+    let { ctx } = this;
+    let { userName, password } = ctx.request.body;
+    let user = await ctx.service.user.login(userName, password);
+    // 结果集长度为0则代表没有该用户
+    if (!user.length) {
+        ctx.body = {
+          code: '004',
+          msg: '用户名或密码错误'
+        }
+        return;
+      }
+      // 数据库设置用户名唯一
+    // 结果集长度为1则代表存在该用户
+    if (user.length == 1) {
+        const loginUser = {
+          user_id: user[0].userId,
+          userName: user[0].userName
+        };
+        // 保存用户信息到session
+        ctx.session.user = loginUser;
+        console.log(ctx.session);
+        ctx.body = {
+          code: '001',
+          user: loginUser,
+          msg: '登录成功'
+        }
+        return;
+      }
   }
   //  注册
   async register() {
     let { ctx } = this;
-    let { userName, password } = ctx.request.body;
-    const result = await ctx.service.user.register(userName, password);
-    ctx.body = result;
+    let { userName, password, userPhoneNumber } = ctx.request.body;
+    let user = await ctx.service.user.find(userName);
+    if (user.length !== 0) {
+        ctx.body = {
+          code: '004',
+          msg: '用户名已经存在，不能注册'
+        }
+        return;
+      }
+
+      try {
+        // 连接数据库插入用户信息
+        let registerResult = await ctx.service.user.register(userName, password, userPhoneNumber);
+        // 操作所影响的记录行数为1,则代表注册成功
+        if (registerResult.affectedRows === 1) {
+          ctx.body = {
+            code: '001',
+            msg: '注册成功'
+          }
+          return;
+        }
+        // 否则失败
+        ctx.body = {
+          code: '500',
+          msg: '未知错误，注册失败'
+        }
+      } catch (error) {
+        reject(error);
+      }
   }
 //  寻找用户
   async find() {
@@ -26,8 +76,8 @@ class UserController extends Controller {
     if (result.length == 0)
     {
         ctx.body = {
-            code: 0,
-            message: '用户名不存在，可以注册'
+            code: '001',
+            msg: '用户名不存在，可以注册'
         }
         return;
     }
@@ -35,8 +85,8 @@ class UserController extends Controller {
     if (result.length == 1)
     {
         ctx.body = {
-            code: 1,
-            message: '用户名已经存在，不能注册'
+            code: '004',
+            msg: '用户名已经存在，不能注册'
         }
         return;
     }
@@ -46,8 +96,8 @@ class UserController extends Controller {
     //返回未知错误
     //正常不会出现
     ctx.body = {
-        code: 500,
-        message: '未知错误'
+        code: '500',
+        msg: '未知错误'
     }
   }
 }
--- app/service/user.js
+++ app/service/user.js
@@ -1,9 +1,16 @@
 const Service = require('egg').Service;
 
 class UserService extends Service {
-    async register(userName, password) {
-        return await this.app.mysql.insert('user', { userName, password });
+    // 登录
+    async login(userName, password) {
+        const sql = 'select * from user where userName = ? and password = ?';
+        return await this.app.mysql.query(sql, [userName, password]);
     }
+    // 注册
+    async register(userName, password, userPhoneNumber) {
+        return await this.app.mysql.insert('user', { userName, password, userPhoneNumber });
+    }
+    // 查找用户
     async find(userName) {
         const user = await this.app.mysql.query('select * from user where userName = ?', userName);
         return user;
